e37aea686e2f6553da265de99d1658903d8a9385
Loh Yie Xuan <lyie-xua@u82z06s03.42kl.edu.my>
2022-09-07 17:59:35 +0800

    C05_1

--- C_05/ex01/ft_recursive_factorial.c
+++ C_05/ex01/ft_recursive_factorial.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyie-xua <lyie-xua@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 21:35:21 by lyie-xua          #+#    #+#             */
+/*   Updated: 2022/09/06 21:36:41 by lyie-xua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	else if	(nb <= 1)
+	else if (nb <= 1)
 		return (1);
 	else
-		return (nb * ft_recursive_factorial(nb - 1)); 
+		return (nb * ft_recursive_factorial(nb - 1));
 }
--- C_05/ex03/ft_recursive_power.c
+++ C_05/ex03/ft_recursive_power.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyie-xua <lyie-xua@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 21:37:13 by lyie-xua          #+#    #+#             */
+/*   Updated: 2022/09/07 11:56:04 by lyie-xua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_recursive_power(int nb, int power)
 {
 	int	res;
@@ -5,8 +17,6 @@ int	ft_recursive_power(int nb, int power)
 	res = nb;
 	if (power > 1)
 		return (nb * ft_recursive_power(nb, (power - 1)));
-	if (power == 0)
-		return (1);
 	if (power < 0)
 		return (0);
 	return (res);
