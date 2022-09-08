1f42162f997709f33cb1f82d05d986367e8d543f
Loh Yie Xuan <lyie-xua@u82z06s03.42kl.edu.my>
2022-09-08 18:25:29 +0800

    C_05

--- C_05/ex02/ft_iterative_power.c
+++ C_05/ex02/ft_iterative_power.c
@@ -6,11 +6,11 @@
 /*   By: lyie-xua <lyie-xua@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 21:37:00 by lyie-xua          #+#    #+#             */
-/*   Updated: 2022/09/06 21:37:02 by lyie-xua         ###   ########.fr       */
+/*   Updated: 2022/09/07 21:36:24 by lyie-xua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterarive_power(int nb, int power)
+int	ft_iterative_power(int nb, int power)
 {
 	int	res;
 
--- C_05/ex03/ft_recursive_power.c
+++ C_05/ex03/ft_recursive_power.c
@@ -6,18 +6,20 @@
 /*   By: lyie-xua <lyie-xua@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 21:37:13 by lyie-xua          #+#    #+#             */
-/*   Updated: 2022/09/07 11:56:04 by lyie-xua         ###   ########.fr       */
+/*   Updated: 2022/09/07 21:41:39 by lyie-xua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
-	int	res;
+	int	result;
 
-	res = nb;
+	result = nb;
 	if (power > 1)
 		return (nb * ft_recursive_power(nb, (power - 1)));
-	if (power < 0)
+	else if (power == 0)
+		return (1);
+	else if (power < 0)
 		return (0);
-	return (res);
+	return (result);
 }
