66b20e59f178ee9833f472bf609902797aa9fdb2
Loh Yie Xuan <lyie-xua@u82z06s03.42kl.edu.my>
2022-09-08 18:28:30 +0800

    C_07

---
+++ C_07/ex00/ft_strdup.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int	i;
+	char	*dest;
+	char	*d
+
+	i = 0;
+	d = ((dest = (char *)malloc(ft_strlen(src) * sizeof(char) + 1)));
+	if (!d)
+		return (0);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
